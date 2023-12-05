742302764f63d11c6f32ba7d1a4ab7eb01cf2fa2
Vincent Jadraque <vincent.jadraque@dfcrc.com>
2023-12-05 14:58:39 +1100

    ci: validate pull request

---
+++ .github/scripts/validate-pr-title.js
@@ -0,0 +1,9 @@
+// validate-pr-title.js
+const conventionalCommitRegex = /^(revert: )?(feat|fix|docs|style|refactor|test|chore)(\(.+\))?: .{1,100}/;
+
+const prTitle = process.argv[2];
+
+if (!conventionalCommitRegex.test(prTitle)) {
+  console.error("error: Pull request title does not follow conventional commit format.");
+  process.exit(1);
+}
